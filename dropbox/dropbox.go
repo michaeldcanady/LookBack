b078994777c171b6acc7a981bde21a9012b9712d
djhunter2016 <David@huntermissions.com>
2020-11-13 16:38:43 -0500

    update to dropbox

--- dropbox/dropbox.go
+++ dropbox/dropbox.go
@@ -1,11 +1,33 @@
 package main
 
 import(
+  "github.com/AlecAivazis/survey/v2"
   "fmt"
 )
 
+func prompts(a *string, b *string){
+  //user prompt for username
+  prompt1 := &survey.Input{
+      Message: "Dropbox username",
+  }
+  survey.AskOne(prompt1, a)
+  //user prompt for password
+  prompt2 := &survey.Password{
+      Message: "Dropbox password",
+  }
+  survey.AskOne(prompt2, b)
+
+  }
+
 func main () {
 
-fmt.Println("Hello World")
+//user string variable
+a := ""
+b := ""
+
+prompts(&a, &b)
+
+fmt.Println(a)
+fmt.Println(b)
 
 }
