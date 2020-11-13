652029a6e1b58892d5edddac8fb15e3fe353695f
Canady <dmcanady@liberty.edu>
2020-11-13 13:53:36 -0500

    Added drive mapping

--- MapDrive/MapDrive.go
+++ MapDrive/MapDrive.go
@@ -1,23 +1,51 @@
-package main
+package MapDrive
 
 import(
   "fmt"
   "os"
+  "os/exec"
+  "path/filepath"
 )
 
-func AvaliableDrives()[]string{
+// Returns a slice of avaliable drives
+func AvaliableDrives(available bool)[]string{
   var r []string
   for _, drive := range "ABCDEFGHIJKLMNOPQRSTUVWXYZ"{
       f, err := os.Open(string(drive)+":\\")
-      if err != nil {
+      if err != nil && available {
           r = append(r, string(drive))
           f.Close()
+      }else if err == nil && available == false{
+        r = append(r, string(drive))
+        f.Close()
       }
   }
   return r
 }
 
+//WORK IN PROGRESS
+//need to find a way to get the name for the drive such as D://AnyUse
+func CheckMapped(){
+  Drives := AvaliableDrives(false)
+  for _,drive := range Drives{
+    fmt.Println(drive,filepath.VolumeName(drive))
+  }
+}
 
-func main(){
-  fmt.Println(AvaliableDrives())
+func MapHDBackupsWindows(username string) bool {
+	temp := "/user:" + username + "@liberty.edu"
+  Drives := AvaliableDrives(true)
+	_, err := exec.Command("net", "use", Drives[0]+":", `\\fs3.liberty.edu\hdbackups`, temp).CombinedOutput()
+	if err != nil {
+		fmt.Println("Error mapping server. Open a CMD", "AS ADMIN", "and run this command:")
+		fmt.Println()
+		fmt.Println("net use M: \\\\fs3.liberty.edu\\hdbackups ", temp)
+		fmt.Println()
+		fmt.Println("It will have you type out your password. If it completed successfully, leave the window open")
+		fmt.Println("and rerun this program")
+		fmt.Println()
+    return false
+	}else{
+    return true
+  }
 }
