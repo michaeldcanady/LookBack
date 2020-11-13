a602195a676b7fd87d79b911786e6e839b4c0033
Canady <dmcanady@liberty.edu>
2020-11-13 17:01:28 -0500

    commmand args

--- command/commandargs.go
+++ command/commandargs.go
@@ -5,20 +5,37 @@ import(
   "fmt"
   "os/user"
   "strings"
+  "path/filepath"
 )
 
 var(
   users string
-  userSlice []string
+  destination string
+  silent bool
+  BorR string
 )
 
 func main(){
+  // Getting current user for default
   currentUser,_ := user.Current()
   User := strings.Split(currentUser.Username,"\\")
+  homedir := currentUser.HomeDir
+  desktop := filepath.Join(homedir,"/Desktop/")
+  // defining flags
   flag.StringVar(&users, "users",User[1], "List all users you want backed up.")
+  flag.StringVar(&destination, "dest",desktop, "Set backup destination.")
+  flag.StringVar(&BorR, "type","backup", "Backup or Restore")
+  flag.BoolVar(&silent, "s",false, "silent command output.")
 
   flag.Parse()
-  
-  userSlice = strings.Split(users," ")
-  fmt.Println(userSlice)
+
+  if BorR != "backup" || BorR != "restore"{
+    panic("-type must be either BACKUP or RESTORE")
+  }
+
+  if !silent{
+    userSlice := strings.Split(users," ")
+    fmt.Println(userSlice)
+    fmt.Println(destination)
+  }
 }
