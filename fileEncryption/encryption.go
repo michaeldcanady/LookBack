e7d6c26689548a9daa7f7bb5a36fcdc3a8c6e060
Canady <dmcanady@liberty.edu>
2020-11-16 11:35:44 -0500

    Added domain option to settings and created query domain function.

--- fileEncryption/encryption.go
+++ fileEncryption/encryption.go
@@ -8,6 +8,7 @@ import(
   "encoding/hex"
   "io/ioutil"
   "encoding/pem"
+  "os/exec"
 )
 
 func StorePublic(key []byte, file string)error{
@@ -44,16 +45,33 @@ func GenerateKey()[]byte{
   return key
 }
 
+// Currently only works for windows
+func GetDomain()(string,error){
+  commandString := "(Get-WmiObject Win32_ComputerSystem).Domain"
+  output, err := exec.Command("Powershell", "-Command", commandString ).CombinedOutput()
+  if err != nil{
+    return "",err
+  }
+  return string(output),nil
+}
+
 func main(){
+  domain, err := GetDomain()
+  if err != nil{
+    fmt.Println(err)
+  }
+  fmt.Println(domain)
+  panic("TEST")
   file := "C:\\go\\src\\github.com\\michaeldcanady\\Project01\\.ssh\\id_rsa.public"
   publicKey := GenerateKey()
-  err := StorePublic(publicKey,file); if err != nil{
+  err = StorePublic(publicKey,file); if err != nil{
     fmt.Println("Storing error:",err)
   }
   publicKey1, err := RetrievePublic(file); if err != nil{
     fmt.Println("Retrieval err:", err)
   }
 
+  // how to encrypt a file returns encrypted bytes to be written
   fileBytes := Encrypt("C:\\Users\\dmcanady\\Desktop\\New folder (3)\\CS6545678_FILE.log",publicKey1)
 
   f, err := os.Create("C:\\Users\\dmcanady\\Desktop\\New folder (3)\\CS6545678_FILE (1).log")
